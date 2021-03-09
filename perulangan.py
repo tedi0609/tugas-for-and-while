fec4c037ec47f2da5c71da2c73bb5f7d586ebd96
Tedi Irawan <52597151+tedi0609@users.noreply.github.com>
2021-03-09 08:04:38 +0800

    Add files via upload

---
+++ perulangan.py
@@ -0,0 +1,14 @@
+#contoh perulangan for
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+  if x == "banana":
+    break
+
+#contoh perulangan while
+i = 1
+while i < 6:
+  print(i)
+  if (i == 3):
+    break
+  i += 1
